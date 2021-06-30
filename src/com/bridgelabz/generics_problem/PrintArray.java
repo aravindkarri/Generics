25793a262f9997dfc8c91ff42e65dd0204b04ab0
aravindkarri <aravindkarri98495@gmail.com>
2021-06-30 20:17:04 +0530

    ADDED: Code to print different datatype arrays using Generics

--- src/com/bridgelabz/generics_problem/PrintArray.java
+++ src/com/bridgelabz/generics_problem/PrintArray.java
@@ -4,45 +4,22 @@ public class PrintArray {
 
 	public static void main(String[] args) {
 
-		int[] intArray = {1,2,3,4,5};
-		double[] doubleArray = {1.1,2.2,3.3,4.4};
-		char[] charArray = {'H','E','L','L','O'};
-
+		Integer[] intArray = {1,2,3,4,5};
+		Double[] doubleArray = {1.1,2.2,3.3,4.4};
+		Character[] charArray = {'H','E','L','L','O'};
+		
 		toPrint(intArray);
 		toPrint(doubleArray);
 		toPrint(charArray);
 
-
 	}
-
-	private static void toPrint(double[] doubleArray)
+	public static <E> void toPrint(E[] inputArray)
 	{
-		for(double element: doubleArray)
+		for(E element: inputArray)
 		{
 			System.out.printf("%s",element);
 		}
 		System.out.println();
-
-	}
-
-	private static void toPrint(int[] intArray) 
-	{
-		for(int element: intArray)
-		{
-			System.out.printf("%s",element);
-		}
-		System.out.println();
-
-
-	}
-	private static void toPrint(char[] charArray)
-	{
-		for(char element: charArray)
-		{
-			System.out.printf("%s",element);
-		}
-		System.out.println();
-
 	}
 
 }
